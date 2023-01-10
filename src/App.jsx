9f415ef5ade1a0daada08aca051c203ee02317a7
Ryan-Zayne <phoenixastro97@gmail.com>
2023-01-10 19:20:30 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { Box, Heading, Text, Flex } from '@chakra-ui/react';
+import Form from './components/Form';
 import './index.css';
 
-function App() {}
-
+function App() {
+	return (
+		<Flex
+			as={'section'}
+			align={'center'}
+			maxW={{ base: '65rem', md: 'unset', lg: '115rem' }}
+			direction={'column'}
+			border={'solid 2rem rgb(17, 24, 40)'}
+			borderRadius={'2rem'}
+			margin={{ base: '3rem auto', md: '4.3rem auto 2rem' }}
+		>
+			<Box
+				as={'header'}
+				bgColor={'rgb(17, 24, 40)'}
+				w={'100%'}
+				paddingBlock={{ base: '0 2rem', md: '1.5rem 3.5rem' }}
+				transform={'translateY(-1px)'}
+			>
+				<Heading as={'h1'} fontSize={'3rem'} color={'hsl(0, 0%, 100%, 0.3)'}>
+					Welcome!
+				</Heading>
+				<Text color={'hsl(0, 0%, 100%, 0.4)'}>This is Chakra-ui in action</Text>
+			</Box>
+			<Form />
+		</Flex>
+	);
+}
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import App from './App';
+
+const extensions = {
+	breakpoints: {
+		sm: '320px',
+		md: '768px',
+		lg: '1000px',
+		xl: '1240px',
+	},
+};
+
+// Extend the theme
+const theme = extendTheme({ ...extensions });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<App />
 		</ChakraProvider>
 	</React.StrictMode>
